b15b17c927e8120994a84d8e61001666f18205fb
Akiva <calculatinghchance2@live.com>
2019-07-22 12:46:43 -0400

    Updating about js random facts

--- action.js
+++ action.js
@@ -13,13 +13,11 @@ var randomfact = document.getElementById('randomfact');
 
 randomfact.addEventListener("click", function (){
   if(x = 0){
-    // document.getElementsById("random1")[0].setAttribute("class", "democlass");
     document.getElementById('random1').style.display = 'block';
     document.getElementById('random3').style.display = 'none';
     x += 1;
   }
   if(x = 1){
-    // document.getElementsById("random1")[0].setAttribute("class", "democlass");
     document.getElementById('random2').style.display = 'block';
     document.getElementById('random1').style.display = 'none';
     x += 1;
@@ -31,10 +29,6 @@ randomfact.addEventListener("click", function (){
     x = 0;
   }
 })
-
-// document.getElementsById("")[0].setAttribute("class", "democlass");
-
-
 
 }
 
