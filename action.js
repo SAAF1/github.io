573625a8ea423cfc01e1cfd2f4f1513bd47e6693
Akiva <calculatinghchance2@live.com>
2019-07-23 15:24:20 -0400

    added actionAbout.js

--- action.js
+++ action.js
@@ -19,29 +19,51 @@ arrow.addEventListener("click", function (){
 // document.getElementById("txtbox") .value=aquote[rdmQuote];
 // }
 
-var x = 0;
-var randomfact = document.getElementById('randomfact');
-
-randomfact.addEventListener("click", function (){
-  if(x == 0){
-    document.getElementById('random1').style.display = 'block';
-    document.getElementById('random3').style.display = 'none';
-    x += 1;
-  }
-  if(x == 1){
-    document.getElementById('random2').style.display = 'block';
-    document.getElementById('random1').style.display = 'none';
-    x += 1;
-  }
-  else
-  {
-    document.getElementById('random3').style.display = 'block';
-    document.getElementById('random2').style.display = 'none';
-    x = 0;
-  }
-});
+// var x = 0;
+// var randomfact = document.getElementById('randomfact');
+//
+// randomfact.addEventListener("click", function (){
+//   if(x == 0){
+//     document.getElementById('random1').style.display = 'block';
+//     document.getElementById('random3').style.display = 'none';
+//     x += 1;
+//   }
+//   if(x == 1){
+//     document.getElementById('random2').style.display = 'block';
+//     document.getElementById('random1').style.display = 'none';
+//     x += 1;
+//   }
+//   else
+//   {
+//     document.getElementById('random3').style.display = 'block';
+//     document.getElementById('random2').style.display = 'none';
+//     x = 0;
+//   }
+// });
 
 };
+
+// var x = 0;
+// var randomfact = document.getElementById('randomfact');
+//
+// randomfact.addEventListener("click", function (){
+//   if(x == 0){
+//     document.getElementById('random1').style.display = 'block';
+//     document.getElementById('random3').style.display = 'none';
+//     x += 1;
+//   }
+//   if(x == 1){
+//     document.getElementById('random2').style.display = 'block';
+//     document.getElementById('random1').style.display = 'none';
+//     x += 1;
+//   }
+//   else
+//   {
+//     document.getElementById('random3').style.display = 'block';
+//     document.getElementById('random2').style.display = 'none';
+//     x = 0;
+//   }
+// });
 
 
 window.onload = function() {
---
+++ actionAbout.js
@@ -0,0 +1,24 @@
+var x = 0;
+var randomfact = document.getElementById('randomfact');
+
+randomfact.addEventListener("click", function (){
+  if(x == 0){
+    document.getElementById('random1').style.display = 'block';
+      document.getElementById('random2').style.display = 'none';
+    document.getElementById('random3').style.display = 'none';
+    x += 1;
+  }
+  if(x == 1){
+    document.getElementById('random2').style.display = 'block';
+    document.getElementById('random1').style.display = 'none';
+    document.getElementById('random3').style.display = 'none';
+    x += 1;
+  }
+  else
+  {
+    document.getElementById('random3').style.display = 'block';
+    document.getElementById('random1').style.display = 'none';
+    document.getElementById('random2').style.display = 'none';
+    x = 0;
+  }
+});
