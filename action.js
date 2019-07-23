d9928c1ebf7503dd25c2b7d753a5f4bcf5456db0
Akiva <calculatinghchance2@live.com>
2019-07-23 12:34:49 -0400

    note that the portfolio is being tinkered with

--- action.js
+++ action.js
@@ -23,12 +23,12 @@ var x = 0;
 var randomfact = document.getElementById('randomfact');
 
 randomfact.addEventListener("click", function (){
-  if(x = 0){
+  if(x == 0){
     document.getElementById('random1').style.display = 'block';
     document.getElementById('random3').style.display = 'none';
     x += 1;
   }
-  if(x = 1){
+  if(x == 1){
     document.getElementById('random2').style.display = 'block';
     document.getElementById('random1').style.display = 'none';
     x += 1;
@@ -39,9 +39,9 @@ randomfact.addEventListener("click", function (){
     document.getElementById('random2').style.display = 'none';
     x = 0;
   }
-})
+});
 
-}
+};
 
 
 window.onload = function() {
