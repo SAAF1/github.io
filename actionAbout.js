7b4d308aed53020120fdc7174b3e1c32c9676e69
Akiva <calculatinghchance2@live.com>
2019-08-15 16:32:40 -0400

    About page has some real bugs still

--- actionAbout.js
+++ actionAbout.js
@@ -33,3 +33,51 @@ randomfact.addEventListener("click", function() {
   }
 
 });
+
+// DESIGN PROCESS
+var y = 0;
+
+var designprocess = document.getElementById('designprocess');
+
+designprocess.addEventListener("click", function() {
+ // var x= 0;
+ switch (y) {
+    case (y = 0):
+      document.getElementById("process1").style.display = "none";
+      document.getElementById("process2").style.display = "block";
+      document.getElementById("process3").style.display = "none";
+      document.getElementById("process4").style.display = "none";
+      console.log(y);
+      y += 1;
+      console.log(y);
+      break;
+    case (y = 1):
+      document.getElementById("process1").style.display = "none";
+      document.getElementById("process2").style.display = "none";
+      document.getElementById("process3").style.display = "block";
+      document.getElementById("process4").style.display = "none";
+      console.log(y);
+      y += 1;
+      console.log(y);
+      break;
+    case (y = 2):
+      document.getElementById("process1").style.display = "none";
+      document.getElementById("process2").style.display = "none";
+      document.getElementById("process3").style.display = "none";
+      document.getElementById("process4").style.display = "block";
+      console.log(y);
+      y += 1;
+      console.log(y);
+      break;
+    case (y = 3):
+      document.getElementById("process1").style.display = "block";
+      document.getElementById("process2").style.display = "none";
+      document.getElementById("process3").style.display = "none";
+      document.getElementById("process4").style.display = "none";
+      console.log(y);
+      y = 0;
+      console.log(y);
+      break;
+  }
+
+});
